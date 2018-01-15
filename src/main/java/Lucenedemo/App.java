7e9a0ac143fcab8aea3f2ad977df7938955e2d2d
Heer <heer.he@ophylink.com>
2018-01-15 17:14:58 +0800

    Initial commit

---
+++ src/main/java/Lucenedemo/App.java
@@ -0,0 +1,13 @@
+package Lucenedemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
